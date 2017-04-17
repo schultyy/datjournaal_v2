7cc2773c711801a41d9d0bb131dbc31fa5188bdf
Jan Schulte <hello@unexpected-co.de>
2017-04-17 12:27:46 +0200

    Render meta controls for each image
    This requires that the user association gets preloaded for each post

--- web/controllers/post_controller.ex
+++ web/controllers/post_controller.ex
@@ -4,7 +4,7 @@ defmodule Datjournaal.PostController do
   alias Datjournaal.Post
 
   def index(conn, _params) do
-    posts = Repo.all(Post)
+    posts = Repo.all(Post) |> Repo.preload(:user)
     render(conn, "index.html", posts: posts)
   end
 
