827d4c3beb7940b867e218806289ed48ca1e24bc
Jan Schulte <hello@unexpected-co.de>
2017-04-18 21:27:40 +0200

    Add Test file for usersettingscontroller

---
+++ test/controllers/user_settings_controller_test.exs
@@ -0,0 +1,25 @@
+defmodule Datjournaal.UserSettingsControllerTest do
+  use Datjournaal.ConnCase
+  import Datjournaal.Factory
+
+  setup do
+    user = insert(:user)
+    user
+    |> build_assoc(:twitterkey)
+    |> Datjournaal.TwitterKey.changeset(%{
+        access_token: "a token",
+        access_token_secret: "A secret",
+        screen_name: "Swift on Development",
+        name: "SwiftOnDev"
+      })
+    |> Repo.insert
+    {:ok, %{user: Repo.preload(user, :twitterkey)}}
+  end
+
+  test "renders user's twitter account details", %{conn: conn, user: user} do
+    conn = conn |> guardian_login(user)
+    response = get conn, user_settings_path(conn, :index)
+    assert html_response(response, 200) =~ user.twitterkey.name
+    assert html_response(response, 200) =~ user.twitterkey.screen_name
+  end
+end
