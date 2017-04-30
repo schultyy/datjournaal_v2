dce5bbf4bcee2c9adc10b99ec660e86281748d9b
Jan Schulte <hello@unexpected-co.de>
2017-04-30 21:34:14 +0200

    Fix rendering of author in detail view. Also render the inserted_at date

--- web/views/post_view.ex
+++ web/views/post_view.ex
@@ -1,8 +1,14 @@
 defmodule Datjournaal.PostView do
   use Datjournaal.Web, :view
+  use Timex
+
   def photo_url(post) do
     foo = Datjournaal.Image.url({post.image, :images}, :original)
           |> Path.basename
     "/uploads/" <> foo
+  end
+
+  def format_time(time) do
+    Timex.format!(time, "%d.%m.%Y %H:%M", :strftime)
   end
 end
