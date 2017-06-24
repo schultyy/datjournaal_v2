3440ba38cfb271e75a67164bfdb97aad649f7eae
Jan Schulte <hello@unexpected-co.de>
2017-06-24 22:41:57 +0200

    Only show stats for unauthenticated visits

--- web/controllers/user_stats_controller.ex
+++ web/controllers/user_stats_controller.ex
@@ -13,22 +13,8 @@ defmodule Datjournaal.UserStatsController do
 
   defp my_posts(user) do
     my_posts = Repo.all(from p in Post, where: p.user_id == ^user.id, select: p.id)
-    Repo.all(from stat in Stat, group_by: stat.post_id, where: stat.post_id in ^my_posts, select: {stat.post_id, count(stat.post_id)})
+    Repo.all(from stat in Stat, group_by: stat.post_id, where: stat.post_id in ^my_posts and stat.authenticated == false, select: {stat.post_id, count(stat.post_id)})
       |> Enum.map(fn({post_id, count}) -> { Repo.get!(Post, post_id), count } end)
       |> Enum.sort(fn({_p1, count1}, {_p2, count2}) -> count1 > count2 end)
   end
-
-  # defp my_posts(user) do
-  #   all_posts_for_user = Repo.all(from p in Post, where: p.user_id == ^user.id, select: p) |> Repo.preload(:user)
-  #   post_slugs = all_posts_for_user |> Enum.map(fn(p) -> p.slug end)
-  #   query = from(st in Stat, group_by: st.path, where: like(st.path, "/api/v1/posts/%") and st.logged_in == false, select: {st.path, count(st.path)})
-  #   Repo.all(query)
-  #     |> Enum.map(fn({path, count}) -> {String.replace_prefix(path, "/api/v1/posts/", ""), count} end)
-  #     |> Enum.filter(fn({path, _count}) -> Enum.member?(post_slugs, path) end)
-  #     |> Enum.map(fn({path, count}) ->
-  #           p = Enum.find(all_posts_for_user, fn(p) -> p.slug == path end)
-  #           {p, count}
-  #         end)
-  #     |> Enum.sort(fn({_p1, count1}, {_p2, count2}) -> count1 > count2 end)
-  # end
 end
