26fa33af975a80e28298e6ed37ef8281f6302df9
Jan Schulte <hello@unexpected-co.de>
2017-07-22 21:58:17 +0200

    Fix hostname in production config

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :datjournaal, Datjournaal.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [host: "datjournaal.de", port: 80],
   uploads_dir: "/var/apps/external/www/uploads",
   cache_static_manifest: "priv/static/manifest.json"
 
