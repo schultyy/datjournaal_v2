6b525dca91959b1e9c7a9f8e1ac017cbb1f31eea
Jan Schulte <hello@unexpected-co.de>
2017-07-23 13:02:14 +0200

    Create the post in setup

--- test/controllers/tracking_controller_test.exs
+++ test/controllers/tracking_controller_test.exs
@@ -2,8 +2,12 @@ defmodule Datjournaal.TrackingControllerTest do
   use Datjournaal.ConnCase
   import Datjournaal.Factory
 
-  test "POST /api/v1/visit/:id returns 200 status code", %{ conn: conn } do
+  setup do
     post = insert(:post)
+    { :ok, post: post }
+  end
+
+  test "POST /api/v1/visit/:id returns 200 status code", %{ conn: conn, post: post } do
     response = post conn, "/api/v1/visit/#{post.slug}"
     assert response.status == 200
   end
