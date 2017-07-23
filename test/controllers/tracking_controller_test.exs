6c88bd65f93f2aafbaba511cee184674afe895dc
Jan Schulte <hello@unexpected-co.de>
2017-07-23 13:21:09 +0200

    Log a visit for an anonymous user

--- test/controllers/tracking_controller_test.exs
+++ test/controllers/tracking_controller_test.exs
@@ -11,4 +11,10 @@ defmodule Datjournaal.TrackingControllerTest do
     response = post conn, "/api/v1/visit/#{post.slug}"
     assert response.status == 200
   end
+
+  test "POST /api/v1/visit/:id creates new log entry", %{ conn: conn, post: post } do
+    post conn, "/api/v1/visit/#{post.slug}"
+    stat = Repo.one(from x in Datjournaal.Stat, order_by: [desc: x.id], limit: 1)
+    assert stat.post_id == post.id
+  end
 end
--- web/controllers/api/v1/tracking_controller.ex
+++ web/controllers/api/v1/tracking_controller.ex
@@ -1,7 +1,29 @@
 defmodule Datjournaal.TrackingController do
   use Datjournaal.Web, :controller
 
-  def log_visit(conn, _params) do
+  alias Datjournaal.{Repo, Post}
+
+  def log_visit(conn, %{ "id" => slug}) do
+    Repo.get_by!(Post, slug: slug)
+    |> create_visit(conn)
     render(conn, "tracking.json", %{})
+  end
+
+  defp create_visit(post, conn) do
+    stats = Datjournaal.Stat.changeset(%Datjournaal.Stat{}, %{
+      unique_identifier: retrieve_ip_address(conn),
+      authenticated: false,
+      post_id: post.id
+    })
+    Repo.insert!(stats)
+  end
+
+  defp retrieve_ip_address(conn) do
+    ip_address = Plug.Conn.get_req_header(conn, "x-forwarded-for")
+                  |> List.first
+    case ip_address do
+      nil -> "127.0.0.1"
+      _ -> ip_address
+    end
   end
 end
