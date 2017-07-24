ca7b3242c83b60cd70875da65539505a01e43bc9
Jan Schulte <hello@unexpected-co.de>
2017-07-24 22:45:25 +0200

    Only render thumbnails for user stats

--- web/views/user_stats.ex
+++ web/views/user_stats.ex
@@ -2,7 +2,7 @@ defmodule Datjournaal.UserStatsView do
   use Datjournaal.Web, :view
 
   def photo_url(post) do
-    foo = Datjournaal.Image.url({post.image, :images}, :original)
+    foo = Datjournaal.Image.url({post.image, :images}, :thumb)
           |> Path.basename
     "/uploads/" <> foo
   end
