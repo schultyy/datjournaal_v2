ac33f4849fd7f36e9a026ce4f78258e89978ccf4
Jan Schulte <hello@unexpected-co.de>
2017-12-28 10:15:58 +0100

    Paginate results

--- test/repositories/index_repository_test.exs
+++ test/repositories/index_repository_test.exs
@@ -7,12 +7,19 @@ defmodule Datjournaal.IndexRepositoryTest do
   test "returns both text and image posts ordered by date" do
     user = insert(:user)
 
-    text_post = insert(:text_post, %{ user: user })
+    insert(:text_post, %{ user: user })
     :timer.sleep(1000)
-    image_post = insert(:post, %{ user: user })
+    insert(:post, %{ user: user })
 
     all = IndexRepository.get_all()
-    assert List.first(all).id == text_post.id
-    assert List.last(all).id == image_post.id
+    assert List.first(all).inserted_at < List.last(all).inserted_at
+  end
+
+  test "returns both text and image posts paginated" do
+    user = insert(:user)
+    insert_list(50, :text_post, %{ user: user })
+    insert_list(50, :post, %{ user: user })
+    all = IndexRepository.get_all(%{ page: 1, page_size: 25 })
+    assert length(all) == 25
   end
 end
--- web/repositories/index_repository.ex
+++ web/repositories/index_repository.ex
@@ -4,6 +4,24 @@ defmodule Datjournaal.IndexRepository do
   alias Datjournaal.TextPost
   alias Datjournaal.Repo
 
+  def get_all(params) do
+    image_posts =
+      ImagePost
+      |> order_by(desc: :inserted_at)
+      |> preload(:user)
+      |> Repo.paginate(params)
+
+    text_posts =
+      TextPost
+      |> order_by(desc: :inserted_at)
+      |> preload(:user)
+      |> Repo.paginate(params)
+
+    all_posts = Enum.concat(image_posts, text_posts)
+    Enum.sort_by(all_posts,  &(&1.inserted_at))
+    |> Enum.take(params[:page_size])
+  end
+
   def get_all() do
     image_posts =
       ImagePost
