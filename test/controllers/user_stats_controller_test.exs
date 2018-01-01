773e5f4b9423b32d6351a0e88aad287ad08c4371
Jan Schulte <hello@unexpected-co.de>
2018-01-01 16:37:58 +0100

    render stats for text posts

--- test/controllers/user_stats_controller_test.exs
+++ test/controllers/user_stats_controller_test.exs
@@ -4,14 +4,22 @@ defmodule Datjournaal.UserStatsControllerTest do
 
   test "renders user stats page", %{conn: conn} do
     user = insert(:user)
-    post = insert(:post, user: user)
+    image_post = insert(:post, user: user)
+    text_post = insert(:text_post, user: user)
 
-    stats = Datjournaal.ImageStat.changeset(%Datjournaal.ImageStat{}, %{
+    image_stats = Datjournaal.ImageStat.changeset(%Datjournaal.ImageStat{}, %{
       unique_identifier: "127.0.0.1",
       authenticated: false,
       image_post_id: post.id
     })
-    Repo.insert!(stats)
+    Repo.insert!(image_stats)
+
+    text_stats = Datjournaal.TextStat.changeset(%Datjournaal.TextStat{}, %{
+      unique_identifier: "127.0.0.1",
+      authenticated: false,
+      text_post_id: text_post.id
+    })
+    Repo.insert!(text_stats)
 
     conn = conn
            |> guardian_login(user)
