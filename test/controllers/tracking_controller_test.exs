49b4361471281c33c85cbfd4db643bdb4590691b
Jan Schulte <hello@unexpected-co.de>
2018-01-01 15:53:11 +0100

    Insert text_posts as well for tracking tests

--- test/controllers/tracking_controller_test.exs
+++ test/controllers/tracking_controller_test.exs
@@ -3,35 +3,36 @@ defmodule Datjournaal.TrackingControllerTest do
   import Datjournaal.Factory
 
   setup do
-    post = insert(:post)
-    { :ok, post: post }
+    image_post = insert(:post)
+    text_post = insert(:text_post)
+    { :ok, image_post: image_post, text_post: text_post }
   end
 
-  test "POST /api/v1/visit/image/:id returns 200 status code", %{ conn: conn, post: post } do
-    response = post conn, "/api/v1/visit/image/#{post.slug}"
+  test "POST /api/v1/visit/image/:id returns 200 status code", %{ conn: conn, image_post: image_post, text_post: _text_post } do
+    response = post conn, "/api/v1/visit/image/#{image_post.slug}"
     assert response.status == 200
   end
 
-  test "POST /api/v1/visit/image/:id creates new log entry", %{ conn: conn, post: post } do
-    post conn, "/api/v1/visit/image/#{post.slug}"
+  test "POST /api/v1/visit/image/:id creates new log entry", %{ conn: conn, image_post: image_post, text_post: _text_post } do
+    post conn, "/api/v1/visit/image/#{image_post.slug}"
     stat = Repo.one(from x in Datjournaal.ImageStat, order_by: [desc: x.id], limit: 1)
-    assert stat.image_post_id == post.id
+    assert stat.image_post_id == image_post.id
     assert stat.authenticated == false
   end
 
-  test "POST /api/v1/visit/image/:id as logged in user returns 200", %{ conn: conn, post: post } do
+  test "POST /api/v1/visit/image/:id as logged in user returns 200", %{ conn: conn, image_post: image_post, text_post: _text_post } do
     current_user = insert(:user)
     conn = conn |> guardian_login(current_user)
-    response = post conn, "/api/v1/visit/image/#{post.slug}"
+    response = post conn, "/api/v1/visit/image/#{image_post.slug}"
     assert response.status == 200
   end
 
-  test "POST /api/v1/visit/image/:id as logged in user logs visit with authenticated == true", %{ conn: conn, post: post } do
+  test "POST /api/v1/visit/image/:id as logged in user logs visit with authenticated == true", %{ conn: conn, image_post: image_post, text_post: _text_post } do
     current_user = insert(:user)
     conn = conn |> guardian_login(current_user)
-    post conn, "/api/v1/visit/image/#{post.slug}"
+    post conn, "/api/v1/visit/image/#{image_post.slug}"
     stat = Repo.one(from x in Datjournaal.ImageStat, order_by: [desc: x.id], limit: 1)
-    assert stat.image_post_id == post.id
+    assert stat.image_post_id == image_post.id
     assert stat.authenticated == true
   end
 end
